18c7674bf63acb6a10697399e1a11ddd42b1769b
Lisa-1919 <lizasimerova438@gmail.com>
2024-10-01 00:18:04 +0300

    feat: add Driver and Car entities with repositories
    - Created Driver entity with fields: id, firstName, lastName, email, phoneNumber, sex, isDeleted, and a relationship to Car
    
    - Created Car entity with fields: id, licensePlate, mark, colour, isDeleted, and a relationship to Driver
    
    - Implemented soft delete for Driver and Car entity using isDeleted field and @SQLDelete annotation
    
    - Added JPA repositories for both Driver and Car entities

---
+++ driver-service/src/main/java/com/example/driver_service/entity/Driver.java
@@ -0,0 +1,55 @@
+package com.example.driver_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.JoinColumn;
+
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import org.hibernate.annotations.SQLDelete;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "driver")
+@SQLDelete(sql = "UPDATE driver SET is_deleted = true WHERE id = ?")
+public class Driver {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "sex")
+    private String sex;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "car_id", referencedColumnName = "id")
+    private Car car;
+
+    @Column(name = "is_deleted")
+    private boolean isDeleted = false;
+
+}
