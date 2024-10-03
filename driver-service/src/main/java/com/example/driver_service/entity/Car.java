a05bbb8bbfd45a8a099e9952038209a4bf549f74
Lisa-1919 <lizasimerova438@gmail.com>
2024-10-03 19:07:35 +0300

    refactor: Replace wildcard imports with explicit imports

--- driver-service/src/main/java/com/example/driver_service/entity/Car.java
+++ driver-service/src/main/java/com/example/driver_service/entity/Car.java
@@ -1,6 +1,12 @@
 package com.example.driver_service.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
