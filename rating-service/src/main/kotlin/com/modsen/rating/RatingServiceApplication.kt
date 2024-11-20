a75c1a34124b5fd97a5868a75878a3335bdb9acb
Lisa-1919 <lizasimerova438@gmail.com>
2024-11-21 00:10:01 +0300

    eureka server implementation

--- rating-service/src/main/kotlin/com/modsen/rating/RatingServiceApplication.kt
+++ rating-service/src/main/kotlin/com/modsen/rating/RatingServiceApplication.kt
@@ -2,10 +2,12 @@ package com.modsen.rating
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 open class RatingServiceApplication
 
 fun main(args: Array<String>) {
--- rides-service/src/main/java/com/modsen/ride/RidesServiceApplication.java
+++ rides-service/src/main/java/com/modsen/ride/RidesServiceApplication.java
@@ -2,10 +2,12 @@ package com.modsen.ride;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "com.example.rides_service.client")
+@EnableFeignClients(basePackages = "com.modsen.ride.client")
+@EnableDiscoveryClient
 public class RidesServiceApplication {
 
 	public static void main(String[] args) {
