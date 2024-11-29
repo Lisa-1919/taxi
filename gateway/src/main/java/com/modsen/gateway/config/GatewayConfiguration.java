7f8a499b02fa5dc9d7b13b8ae6c15619af2925cc
Lisa-1919 <85586004+Lisa-1919@users.noreply.github.com>
2024-11-29 12:59:47 +0300

    Add API Gateway (#21)
    * Add API Gateway
    
    * refactor GatewayConfiguration to use @Value annotations for injecting route URIs from application.properties

---
+++ gateway/src/main/java/com/modsen/gateway/config/GatewayConfiguration.java
@@ -0,0 +1,37 @@
+package com.modsen.gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfiguration {
+
+    @Value("${route.driver-service.uri}")
+    private String driverServiceUri;
+
+    @Value("${route.passenger-service.uri}")
+    private String passengerServiceUri;
+
+    @Value("${route.rating-service.uri}")
+    private String ratingServiceUri;
+
+    @Value("${route.rides-service.uri}")
+    private String ridesServiceUri;
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("driver-service", r->r.path("/api/v1/drivers/**")
+                        .uri(driverServiceUri))
+                .route("passenger-service", r->r.path("/api/v1/passengers/**")
+                        .uri(passengerServiceUri))
+                .route("rating-service", r->r.path("/api/v1/rates/**")
+                        .uri(ratingServiceUri))
+                .route("rides-service", r->r.path("/api/v1/rides/**")
+                        .uri(ridesServiceUri))
+                .build();
+    }
+}
